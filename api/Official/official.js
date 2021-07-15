df51f8360102e4bce578c600d0ffc434c61ab0c7
deliciousFish <32774371+ikl258794613@users.noreply.github.com>
2021-07-15 17:05:18 +0800

    create drit

--- api/Official/official.js
+++ api/Official/official.js
@@ -4,13 +4,8 @@ const connection = require("../database/db");
 
 router.get("/", async (req, res) => {
   let productresults = await connection.queryAsync("SELECT * FROM official_product");//官方商品資料
-
   console.log(productresults)
-  //或許能改成promise.all一次拿3筆資料再送出
   res.json(productresults);
-  // res.send("node")
-  // res.json(ingredientresults);
-  // res.json(seriesresults);
 });
 
 module.exports = router;
